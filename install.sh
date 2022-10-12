44d5c8106c7a4071d721410b5886d6ede349009f
brdelphus <delphus@delphus.org>
2022-10-12 11:14:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,11 +62,11 @@
         rm $WGUI_BIN_PATH/wireguard-ui
       fi
       ln -s $WGUI_PATH/wireguard-ui $WGUI_BIN_PATH/wireguard-ui
-      
+ 
+      cd /tmp
       curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
       unzip awscliv2.zip
       cd aws && sudo ./install
-      mkdir -p /root/.aws
       cd -
       
       systemctl stop apache2
